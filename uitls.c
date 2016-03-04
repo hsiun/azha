4b627e70ac1edb2272becfabce780085ac0df960
Xiong Gao <growdane@gmail.com>
2016-03-04 23:20:03 +0800

    add network warpper

--- uitls.c
+++ uitls.c
@@ -1,8 +1,150 @@
 #include "utils.h"
 
+
+/*****************
+ * IO处理
+ ****************/
+ssize_t readn(int fd, void *vptr, size_t n)
+{
+    size_t nleft;   //文件描述符中剩下的
+    ssize_t nread;  //已读的
+    char *ptr;
+
+    ptr = vptr;
+    nleft = n;
+    while (nleft > 0){
+        if ( (nread = read(fd, ptr, nleft)) < 0){
+            if (errno == EINTR)
+                nread = 0;      //读入过程被系统中断，重新调用read
+            else
+                return (-1);
+        } else if (nread == 0)
+            break;               //EOF
+        nleft -= nread;
+        ptr += nread;
+    }
+    return (n - nleft);
+}
+
+ssize_t writen(int fd, const void *vptr, size_t n)
+{
+    size_t nleft;    //描述符中没有被写入的
+    ssize_t nwritten; //已希尔到缓冲中的
+    const char *ptr;
+
+    ptr = vptr;
+    nleft = n;
+    while (nleft > 0){
+        if ( (nwritten = write(fd, ptr, nleft)) <= 0){
+            if (nwritten < 0 && errno == EINTR)
+                nwritten = 0;                //写入被中断，重新写
+            else
+                return (-1);                //调用write出错，立即返回
+        }
+        nleft -= nwritten;
+        ptr += nwritten;
+    }
+    return (n);
+}
+
+/* 采用循环读入一个字符的方式读入行，被原作者标记为极慢的 */
+ssize_t readline(int fd, void *vptr, size_t maxlen)
+{
+    ssize_t n, rc; //n是循环计数器，rc读入的数据量
+    char c, *ptr;
+
+    ptr = vptr;
+    for (n = 1; n < maxlen; n++) {
+    again:
+        if ( (rc = read(fd, &c, 1)) == 1) {
+            *ptr++ = c;
+            if (c == '\n')
+                break;
+        } else if (rc == 0) {
+            *ptr = 0;
+            return (n - 1);
+        } else {
+            if (errno == EINTR)
+                goto again;
+            return (-1);
+        }
+    }
+
+    *ptr = 0;
+    return (n);
+}
+
+
 /*****************
  * 包裹函数
  ******************/
+/* 网络包裹函数 */
+int Socket(int family, int type, int protocol)
+{
+    int rc;
+
+    if ( (rc = socket(family, type, protocol)) < 0)
+        unix_error("Socket error");
+    return rc;
+}
+
+int Connect(int sockfd, const struct sockaddr *serv_addr, socklen_t addr_len)
+{
+    int rc;
+    
+    if ( (rc = connect(sockfd, serv_addr, addr_len)) < 0)
+        unix_error("Connect error");
+
+    return rc;
+}
+
+int Bind(int sockfd, const struct sockaddr *myaddr, socklen_t addr_len)
+{
+    int rc;
+    if ( (rc = bind(sockfd, myaddr, addr_len)) < 0)
+        unix_error("Listen error");
+
+    return rc;
+}
+
+int Listen(int s, int backlog)
+{
+    int rc;
+
+    if ( (rc = listen(s, backlog)) < 0)
+        unix_error("Listen error");
+
+    return rc;
+}
+
+int Accept(int sockfd, struct sockaddr *cliaddr, socklen_t *addr_len)
+{
+    int rc;
+
+    if ( (rc = accept(sockfd, cliaddr, addr_len)) < 0)
+        unix_error("Accept error");
+
+    return rc;
+}
+
+/* 系统调用的包裹函数 */
+pid_t Fork(void)
+{
+    pid_t pid;
+
+    if ( (pid = fork()) < 0)
+        unix_error("Fork error");
+
+    return pid;
+}
+
+int Close(int fd)
+{
+    int rc;
+    if ( (rc = close(fd)) < 0)
+        unix_error("Close error");
+    
+}
 /* 错误处理函数 */
 void unix_error(char *msg)
 {
--- utils.h
+++ utils.h
@@ -22,11 +22,28 @@
 #define MAXLINE (8192)
 #define SERVER_PORT (8800)  
 
+/*****************************
+ * IO处理
+ ****************************/
+ssize_t readn(int filedes, void *buff, size_t nbytes);            //从描述符中读入nbytes个字符到buff中
+ssize_t writen(int filedes, const void *buff, size_t nbytes);     //从buff中写入nbytes个字符到描述符中
+ssize_t readline(int filedes, void *buff, size_t maxlen);         //行处理，极慢
+
 
 /******************************
  * 包裹函数的定义
  * 分成错误，系统，网络三部分定义
  *******************************/
+/* 网络 */
+int Socket(int family, int type, int protocol);
+int Connect(int sockfd, const struct sockaddr *servaddr, socklen_t addrlen);
+int Bind(int sockfd, const struct sockaddr *myaddr, socklen_t addrlen);
+int Listen(int sockfd, int backlog);
+int Accept(int sockfd, struct sockaddr *cliaddr, socklen_t *addrlen);
+
+/* 系统 */
+pid_t Fork(void);
+int Close(int sockfd);
 
 /* 错误处理 */
 void unix_error(char *msg);
